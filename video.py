c7a0c9e3c3d87a887a8c66b375e40a8ce7001fb0
EscobarWilliam <williamj915@gmail.com>
2024-01-17 21:44:32 -0800

    video push

--- video.py
+++ video.py
@@ -7,17 +7,10 @@ while(1):
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # define range of blue color in HSV
-    lower_grey = np.array([0,0,50])
-    upper_grey = np.array([179,30,220])
-    
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
-    
-    lower_yellow = np.array([20, 100, 100])
-    upper_yellow = np.array([30, 255, 255])    
-    
     # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue, lower_grey, upper_grey, lower_yellow, upper_yellow)
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
     res = cv.bitwise_and(frame,frame, mask= mask)
     cv.imshow('frame',frame)
