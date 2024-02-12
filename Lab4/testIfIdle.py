fe260f19f2ef229f5e2468130a58188e13ae2c37
EscobarWilliam <williamj915@gmail.com>
2024-02-11 18:07:46 -0800

    -

--- Lab4/testIfIdle.py
+++ Lab4/testIfIdle.py
@@ -1,60 +1,3 @@
-# import paho.mqtt.client as mqtt
-# import json
-
-# # Define global variables to store data
-# prev_acc = {"ACC_X": 0, "ACC_Y": 0, "ACC_Z": 0}
-# threshold = 0.1  # You can adjust this threshold as needed
-
-# '''*************************************************************************************
-# //                               def is_idle()
-# // This is a function, that when called, will determine if the IMU is idle or not.  
-# // The way it achieves this is by taking the difference between the current data 
-# // and the previous reading. This is simply a test, but for a game where our players
-# // are constantly moving, we would need to adjust the treshold at which we detect a 
-# // motion.  
-# //*************************************************************************************'''
-# def is_idle(data):
-#     global prev_acc
-#     try:
-#         #we measure our data by taking the difference between 
-#         #present and past readings
-#         delta_x = abs(data["ACC_X"] - prev_acc["ACC_X"])
-#         delta_y = abs(data["ACC_Y"] - prev_acc["ACC_Y"])
-#         delta_z = abs(data["ACC_Z"] - prev_acc["ACC_Z"])
-#         prev_acc = data  # Update previous acceleration values
-#         if delta_x < threshold and delta_y < threshold and delta_z < threshold:
-#             return True #i.e. is idle 
-#         else:
-#             return False
-#     except Exception as e:
-#         print("Error checking acceleration:", e)
-#         return False
-
-# '''//*************************************************************************************
-# //                             Callback functions
-# //*************************************************************************************'''
-# def on_connect(client, userdata, flags, rc):
-#     print("Connected with result code "+str(rc))
-#     client.subscribe("ece180da/william/lab4/imu")
-
-
-# def on_message(client, userdata, msg):
-#     try:
-#         data = json.loads(msg.payload.decode())
-#         if is_idle(data):
-#             print("IDLE")
-#         else:
-#             print("NOT IDLE")
-#     except Exception as e:
-#         print("Error processing message:", e)
-
-
-# client = mqtt.Client()
-# client.on_connect = on_connect
-# client.on_message = on_message
-
-# client.connect_async('mqtt.eclipseprojects.io')
-# client.loop_forever()
 import paho.mqtt.client as mqtt
 import json
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -68,6 +11,14 @@ threshold = 0.1  # You can adjust this threshold as needed
 true_labels = []
 predicted_labels = []
 
+'''*************************************************************************************
+//                               def is_idle()
+// This is a function, that when called, will determine if the IMU is idle or not.  
+// The way it achieves this is by taking the difference between the current data 
+// and the previous reading. This is simply a test, but for a game where our players
+// are constantly moving, we would need to adjust the treshold at which we detect a 
+// motion.  
+//*************************************************************************************'''
 def is_idle(data):
     global prev_acc
     try:
@@ -83,6 +34,9 @@ def is_idle(data):
         print("Error checking acceleration:", e)
         return False
 
+'''//*************************************************************************************
+//                             Callback functions
+//*************************************************************************************'''
 def on_connect(client, userdata, flags, rc):
     print("Connection returned result: " + str(rc))
     
